19b1ede1722739804d2e2fb8eb935702a384cc9f
Felipe almeida <felipepereira937@gmail.com>
2018-09-11 17:11:18 -0300

    refact: func fieldsToArray

--- src/json-replace.ts
+++ src/json-replace.ts
@@ -1,8 +1,7 @@
 export default class JsonReplace {
   fieldsToArray(fieldsExpression: String): Array<String> {
-    const string_splitted: Array<String> = fieldsExpression.split(';');
-    
-    return string_splitted
-      .filter(elt => elt != '');
+    return fieldsExpression
+      .split(';')
+      .filter(elt => elt != '')
   }
 }
