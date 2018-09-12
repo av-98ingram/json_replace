21d58aebf6916f2049d31e34341c17674aefb2c6
Felipe almeida <felipepereira937@gmail.com>
2018-09-12 11:21:33 -0300

    func that get a value

--- src/json-replace.ts
+++ src/json-replace.ts
@@ -1,7 +1,31 @@
 export default class JsonReplace {
-  fieldsToArray(fieldsExpression: String): Array<String> {
+  fieldsToArray(fieldsExpression: string): Array<string> {
     return fieldsExpression
       .split(';')
       .filter(elt => elt != '')
   }
+  
+  searchValue(json: Object, field: string, callback: any): any {
+    const filterFieldStr: any = (element, index, arr) => index != 0
+    const arrFields: Array<string> = field.split('.')
+
+    if (arrFields.length > 1) {
+      const newField: string = arrFields[0]
+      const newJson: string = arrFields
+        .filter(filterFieldStr)
+        .join('.')
+
+      this.searchValue(json[newField], newJson, callback)
+    } else {
+      callback(json[field])
+    }
+  }
+
+  // replace(json: Object, fields: string, callback: any): Promise<object> {
+  //   const arrOfFields: Array<string> = this.fieldsToArray(fields)
+  //   const jsonReplaced: object = arrOfFields
+  //     .map(field => this.searchValue(json, field, callback))
+
+  //   return new Promise((resolve, reject) => resolve({a: { a: 15 },b: 10}))
+  // }
 }
--- tests/parse_json.test.ts
+++ tests/parse_json.test.ts
@@ -1,7 +1,7 @@
 import JsonReplace from '../src/json-replace';
 
 test('function that transform field string to array', () => {
-  const jsonReplace = new JsonReplace()
+  const jsonReplace: JsonReplace = new JsonReplace()
 
   expect(jsonReplace.fieldsToArray('field1;field2'))
     .toEqual(['field1', 'field2'])
@@ -13,3 +13,43 @@ test('function can`t return a empty', () => {
   expect(jsonReplace.fieldsToArray('field1;field2;'))
     .not.toEqual(['field1', 'field2', ''])
 })
+
+test('test: get value from JSON', done => {
+  const jsonReplace: JsonReplace = new JsonReplace()
+  const json: Object = { a: 'value a', b: 'value b' }
+  const func: Object = value => {
+    expect(value).toEqual('value b');
+    done();
+  }
+
+  jsonReplace.searchValue(json, 'b', func)
+})
+
+test('test: get value from a key with child', done => {
+  const jsonReplace: JsonReplace = new JsonReplace()
+  const child: Object = { a: 10 }
+  const json: Object = { a: child, b: 'value b' }
+  const func: Object = value => {
+    expect(value).toEqual(10);
+    done();
+  }
+
+  jsonReplace.searchValue(json, 'a.a', func)
+})
+
+// test('test: replace values', done => {
+//   const jsonReplace: JsonReplace = new JsonReplace()
+//   const child: Object = { a: 10 }
+//   const json: Object = { a: child, b: 5 }
+//   const fields: string = 'a.a;b'
+//   const func: Object = value => value + 5
+//   const expectedResult: Object = {a: { a: 15 },b: 10}
+
+//   jsonReplace.replace(json, fields, func)
+//     .then(result => {
+//       expect(result)
+//         .toEqual(expectedResult)
+      
+//       done()
+//     });
+// })
